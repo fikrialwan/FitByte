3e43287b3ea6aaa0cf37265a6c7dbb9c17944927
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2025-09-03 22:11:56 +0700

    fix(database): add verify db variables & db connections

--- config/database.go
+++ config/database.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -14,10 +15,21 @@ func RunExtension(db *gorm.DB) {
 	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
 }
 
+func validateRequiredEnvVars() {
+	required := []string{"DB_USER", "DB_PASS", "DB_HOST", "DB_NAME", "DB_PORT", "JWT_SECRET"}
+	for _, env := range required {
+		if os.Getenv(env) == "" {
+			panic(fmt.Sprintf("Required environment variable %s is not set", env))
+		}
+	}
+}
+
 func InitDb() *gorm.DB {
 	if err := godotenv.Load(); err != nil {
 		panic(err)
 	}
+
+	validateRequiredEnvVars()
 
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
@@ -30,8 +42,17 @@ func InitDb() *gorm.DB {
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN: dsn,
 	}), &gorm.Config{})
+
 	if err != nil {
 		panic(err)
+	}
+
+	sqlDB, err := db.DB()
+	if err == nil {
+		sqlDB.SetMaxIdleConns(50)
+		sqlDB.SetMaxOpenConns(200)
+		sqlDB.SetConnMaxLifetime(time.Hour)
+		sqlDB.SetConnMaxIdleTime(30 * time.Minute)
 	}
 
 	RunExtension(db)
